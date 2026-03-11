a65c453cb4273b6dd6b19ec624deee99eaba6991
Edgar Miguel Rivera Amaro <20253rd025@utez.edu.mx>
2026-03-11 09:48:15 -0600

    Se agrega el proyecto

--- demolistviewfile/src/main/java/module-info.java
+++ demolistviewfile/src/main/java/module-info.java
@@ -1,7 +1,15 @@
-module com.example {
+module com.example.demolistviewfile {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.example to javafx.fxml;
-    exports com.example;
+
+    opens com.example.demolistviewfile to javafx.fxml;
+    opens com.example.demolistviewfile.services to javafx.fxml;
+    opens com.example.demolistviewfile.repositories to javafx.fxml;
+    opens com.example.demolistviewfile.controllers to javafx.fxml;
+    exports com.example.demolistviewfile;
+    exports com.example.demolistviewfile.services;
+    exports com.example.demolistviewfile.controllers;
+    exports com.example.demolistviewfile.repositories;
+
 }
